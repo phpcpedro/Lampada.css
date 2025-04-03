c955a9ce88f4ff872acf36356536f86614e682b2
phpcpedro <126592396+phpcpedro@users.noreply.github.com>
2025-04-03 09:58:29 -0300

    Add files via upload

---
+++ pedroh.js
@@ -0,0 +1,56 @@
+//  Exiba a mensagem "Olá, Mundo!" no console. //
+console.log ("Olá Mundo")
+
+// Crie uma variável com seu nome e exiba o valor no console. //
+let nome = "Pedro";
+console.log(nome);
+
+//  Some dois números e exiba o resultado. //
+let num1 = 5;
+let num2 = 3;
+console.log (num1 + num2);
+
+//  Subtraia dois números e exiba o resultado. //
+console.log (num1 - num2);
+
+// Mltiplique dois números e exiba o resultado. //
+console.log (num1 * num2);
+
+// Divida dois números e exiba o resultado. //
+console.log (num1 / num2);
+
+// Descubra o resto da divisão de um número por outro. //
+console.log (num1 % num2)
+
+// Crie uma variável booleana (true ou false) e exiba seu valor. //
+let booleano = true;
+console.log (booleano);
+
+// Verifique se um número é maior que o outro e exiba o resultado. //
+console.log (num1 > num2);
+
+// Junte duas palavras (strings) e exiba o resultado. //
+let palavra1 = "Olá";
+let palavra2 = "Mundo";
+console.log(palavra1 + " " + palavra2);
+ 
+//  Converta uma string que representa um número para um tipo numérico. // 
+let str = "123";
+let numConvertido = parseInt(str);
+console.log(numConvertido);
+
+// Verifique se um número é par ou ímpar. //
+if (num1 % 2 === 0) {
+    console.log(num1 + "é par");
+} else  {
+    console.log(num1 + "é ímpar");
+}
+
+// Crie um array com três frutas e exiba a segunda fruta. //
+let frutas = ["banana", "maçã", "laranja"];
+console.log(frutas[1]);
+
+//  Adicione um novo número ao final de um array e exiba o array atualizado. //
+let numeros = [1, 2, 3];
+numeros.push(4);
+console.log(numeros);
